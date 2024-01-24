cba3c07e97d8a71325f27c9ee5388be77ec7a654
Samantha S <148943367+PastelBun@users.noreply.github.com>
2024-01-24 09:16:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let to_top_btn=document.getElementById(´to_top');
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
